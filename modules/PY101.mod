efa2061486fabc4686eda5d0bb86fce6d013ba84
Ryan Griffin <ryangrif@gmail.com>
2017-07-24 23:20:50 -0400

    Add modules folder

---
+++ modules/PY101.mod
@@ -0,0 +1,4 @@
++ Rigging_Tool 1.0 C:\Users\Griffy\Documents\GitHub\Python_101
+RIGGING_TOOL=C:\Users\Griffy\Documents\GitHub\Python_101
+DATA_PATH=C:\Users\Griffy\Documents\GitHub\Python_101\data
+scripts: C:\Users\Griffy\Documents\GitHub\Python_101
--- startup.py
+++ startup.py
@@ -12,9 +12,6 @@ cmds.currentUnit( time='ntsc' )
 # Change the current linear unit to inches
 cmds.currentUnit( linear='cm' )
 
-# Set a system path to files.  We can do this with the os module
-os.environ["RDOJO_DATA"] = os.environ['RIGGING_TOOL'] + '/data'
-
 import ui.ui as ui
 reload(ui)
 ui.RDojo_UI()
