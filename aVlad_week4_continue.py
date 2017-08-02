382b6f7c7fb8125cfb2ff44d76963f199831232e
VladlenAf <tokyofirstbay@gmail.com>
2017-08-02 23:35:16 +0900

    aVlad_week4_update(02.08)
    I got problem here with "ctrlMaking" function.

--- aVlad_week4_continue.py
+++ aVlad_week4_continue.py
@@ -12,24 +12,28 @@ jntDict = {"leftArm" : [
 ["rt_wrist_JNT", [-37.7333821711548, 83.04211007076694, -0.7054100000000001]], 
 ["rt_wristEnd_JNT",[-59.84439, 61.86297, 1.30086]]
 ]}
+#IK Ctrl List
+lf_IKctrlList = [jntDict["leftArm"][0][1], "IK_lf_arm_CTRL", "IK_lf_arm_CTRL_GRP"]
+rt_IKctrlList = [jntDict["rightArm"][0][1], "IK_rt_arm_CTRL", "IK_rt_arm_CTRL_GRP"]
+
 #Building joints___Function
 def create_arm_jnt(jntType):
-    for i in jntDict:
-        for joint in jntDict[i]:
+    for jnt in jntDict:
+        for joint in jntDict[jnt]:
             cmds.joint(n=jntType+joint[0],p=joint[1])
             cmds.select(d=True)
             
 #Orient Joints____Function
 def arm_orientJoint(orntJnt, aimJnt, aimVec, upVec):
-     cmds.spaceLocator()
-     cmds.parent("locator1", orntJnt)
-     cmds.setAttr("locator1.translate", 0, 0, 5, type = "double3")
-     cmds.parent("locator1", w=True)
+     loc = cmds.spaceLocator()
+     cmds.parent(loc[0], orntJnt)
+     cmds.setAttr(loc[0]+".translate", 0, 0, 5, type = "double3")
+     cmds.parent(loc[0], w=True)
      cmds.aimConstraint(aimJnt, orntJnt, offset = [0, 0, 0], weight = True, aimVector = aimVec ,
-                                             upVector = upVec, worldUpType = "object" , worldUpObject = "locator1")
+                                             upVector = upVec, worldUpType = "object" , worldUpObject = loc[0])
      cmds.delete(orntJnt+"_aimConstraint1")
      cmds.makeIdentity(orntJnt, apply = True, t = 0,  r = 1, s = 0, n = 0, pn = True)
-     cmds.delete("locator1")
+     cmds.delete(loc[0])
      cmds.parent(aimJnt, orntJnt)
      
 vecList = ([1, 0, 0], [-1, 0, 0], [0, 1, 0], [0, -1, 0])     
@@ -46,6 +50,15 @@ def cleanJntOrient(jntType, side):
         else:
             cmds.setAttr(jntType + jntDict[side][i][0] +".jointOrientX", 0)
             cmds.setAttr(jntType + jntDict[side][i][0] +".jointOrientY", 0)
+
+#Making Controls___Function
+def ctrlMaking(ctrlinfo):
+    for info in ctrlinfo:
+        pos = info[0]
+        ctrlGRP = cmds.group(em = True, name = info[2])
+        ctrl = cmds.circle(n=info[1], nr=(0, 0, 1), c=(0, 0, 0))
+        cmds.parent(ctrl, ctrlGRP)
+        cmds.xform(ctrlGRP, t=pos, ws =True)
 
 
 
@@ -75,7 +88,7 @@ arm_orientJoint("FK_rt_shoulder_JNT","FK_rt_elbow_JNT", vecList[1], vecList[3])
 arm_orientJoint("FK_rt_elbow_JNT","FK_rt_wrist_JNT", vecList[1], vecList[3])
 arm_orientJoint("FK_rt_wrist_JNT","FK_rt_wristEnd_JNT", vecList[1], vecList[3])
 
-#Cleaning Joint Orient
+#Cleaning Joint Orient 
 cleanJntOrient("IK_", "leftArm")
 cleanJntOrient("FK_", "leftArm")
 cleanJntOrient("bn_", "leftArm")
@@ -83,19 +96,19 @@ cleanJntOrient("bn_", "leftArm")
 cleanJntOrient("IK_", "rightArm")
 cleanJntOrient("FK_", "rightArm")
 cleanJntOrient("bn_", "rightArm")
-########################################################################################################################
 
+#Making IK Handle 
+lf_IKH = cmds.ikHandle( n = "rIK_lf_arm_Hndl" , sj = "IK_" + jntDict["leftArm"][0][0], ee = "IK_" + jntDict["leftArm"][2][0])
+lf_IKE = cmds.rename(lf_IKH[1], "rIK_lf_arm_Eff")
+rt_IKH = cmds.ikHandle( n = "rIK_rt_arm_Hndl" , sj = "IK_" + jntDict["rightArm"][0][0], ee = "IK_" + jntDict["rightArm"][2][0])
+rt_IKE = cmds.rename(rt_IKH[1], "rIK_rt_arm_Eff")
 
+#Orienting IK Handle
+cmds.parent(lf_IKH[0], "bn_"+jntDict["leftArm"][2][0])
+cmds.parent(rt_IKH[0], "bn_"+jntDict["rightArm"][2][0])
+cmds.makeIdentity(lf_IKH[0], apply = True, t = 0,  r = 1, s = 0, n = 0, pn = True)
+cmds.makeIdentity(rt_IKH[0], apply = True, t = 0,  r = 1, s = 0, n = 0, pn = True)
+cmds.parent(lf_IKH[0], w = True)
+cmds.parent(rt_IKH[0], w = True)
 
-#IK Handle making and control for (Right and Left Side)
-cmds.ikHandle( n = "rIK_lf_arm_Hndl" , sj = "IK_lf_shoulder_JNT", ee = "IK_lf_wrist_JNT")
-cmds.rename("effector1", "rIK_lf_arm_Eff")
-cmds.ikHandle( n = "rIK_rt_arm_Hndl" , sj = "IK_rt_shoulder_JNT", ee = "IK_rt_wrist_JNT")
-cmds.rename("effector1", "rIK_rt_arm_Eff")
-cmds.parent("rIK_lf_arm_Hndl", "bn_lf_wrist_JNT" )
-cmds.parent("rIK_rt_arm_Hndl", "bn_rt_wrist_JNT" )
-cmds.makeIdentity("rIK_lf_arm_Hndl", apply = True, t = 0,  r = 1, s = 0, n = 0, pn = True)
-cmds.makeIdentity("rIK_rt_arm_Hndl", apply = True, t = 0,  r = 1, s = 0, n = 0, pn = True)
-cmds.parent("rIK_lf_arm_Hndl", w = True)
-cmds.parent("rIK_rt_arm_Hndl", w = True)
-
+ctrlMaking(lf_IKctrlList)
