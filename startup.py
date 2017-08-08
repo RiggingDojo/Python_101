4361de357f8dc21e8649a498d31b2305c3013305
christian-corsica <christian.corsica@gmail.com>
2017-08-08 08:47:29 -0500

    Add files via upload

---
+++ startup.py
@@ -0,0 +1,24 @@
+import maya.cmds as cmds
+import os
+import system.utils as utils
+#reload(utils)
+
+print "Startup"
+
+
+# Change the current time unit to ntsc
+cmds.currentUnit( time='ntsc' )
+
+# Change the current linear unit to inches
+cmds.currentUnit( linear='cm' )
+
+# Set a system path to files.  We can do this with the os module
+#os.environ["RDOJO_DATA"] = os.environ['RIGGING_TOOL'] + '/data'
+
+import ui.ui as ui
+#reload(ui)
+<<<<<<< HEAD
+ui.RDojo_UI()
+=======
+ui.RDojo_UI()
+>>>>>>> d1836a3bd3c8fb31fef81ae6556625de0a54dbd8
