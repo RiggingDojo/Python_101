d1836a3bd3c8fb31fef81ae6556625de0a54dbd8
christian-corsica <christian.corsica@gmail.com>
2017-08-08 00:26:52 -0500

    Update startup.py

--- startup.py
+++ startup.py
@@ -1,7 +1,7 @@
 import maya.cmds as cmds
 import os
 import system.utils as utils
-reload(utils)
+#reload(utils)
 
 print "Startup"
 
@@ -13,7 +13,7 @@ cmds.currentUnit( time='ntsc' )
 cmds.currentUnit( linear='cm' )
 
 # Set a system path to files.  We can do this with the os module
-os.environ["RDOJO_DATA"] = os.environ['RIGGING_TOOL'] + '/data'
+#os.environ["RDOJO_DATA"] = os.environ['RIGGING_TOOL'] + '/data'
 
 import ui.ui as ui
 #reload(ui)
