698080ee100a1c464b36814b42dbceaaf9caf98e
VladlenAf <tokyofirstbay@gmail.com>
2017-08-23 21:54:52 +0900

    Update

--- rig/rig_arm.py
+++ rig/rig_arm.py
@@ -16,115 +16,106 @@ rigData["FK_ArmList"] = [
                          ["FK_lf_shoulder_CTRL", "FK_lf_shoulder_CTRL_srt", "FK_lf_elbow_CTRL", "FK_lf_elbow_CTRL_srt", "FK_lf_wrist_CTRL", "FK_lf_wrist_CTRL_srt"], 
                          ["FK_rt_shoulder_CTRL", "FK_rt_shoulder_CTRL_srt", "FK_rt_elbow_CTRL", "FK_rt_elbow_CTRL_srt", "FK_rt_wrist_CTRL", "FK_rt_wrist_CTRL_srt"]
                          ]
-rigData["HierarchyOrg"] = ["singer", "singer_GEO_hrc", "GlobalMove_CTRL_srt", "JNT_hrc", "IK_skeleton_hrc", "FK_skeleton_hrc", "skeleton_hrc", "extra_JNT_hrc", "ControlObjects_hrc", "ExtraNode_hrc",
-"toHide_hrc", "toShow_hrc"]                    
-
+rigData["HierarchyOrg"] = ["singer", "singer_GEO_hrc", "GlobalMove_CTRL_srt", "JNT_hrc", "skeleton_hrc", "extra_JNT_hrc", "ControlObjects_hrc", "ExtraNode_hrc",
+"toHide_hrc", "toShow_hrc", "IKh_hrc"]                   
 
 class Rig_Arm:
     def rig_arm(self):
         self.HrcSetup()
-            #Building joints
+             
         self.createArmJnt("IK_")
         self.createArmJnt("FK_")
         self.createArmJnt("bn_")
 
-            #Orient Joints
-        vecList = ([1, 0, 0], [-1, 0, 0], [0, 1, 0], [0, -1, 0]) 
-        self.arm_orientJoint("bn_"+rigData["ArmJoints"][0][0],"bn_"+rigData["ArmJoints"][0][1], vecList[0], vecList[2])
-        self.arm_orientJoint("bn_"+rigData["ArmJoints"][0][1],"bn_"+rigData["ArmJoints"][0][2], vecList[0], vecList[2])
-        self.arm_orientJoint("bn_"+rigData["ArmJoints"][0][2],"bn_"+rigData["ArmJoints"][0][3], vecList[0], vecList[2])
+        vecList = ([1, 0, 0], [-1, 0, 0], [0, 1, 0], [0, -1, 0])
+        self.arm_orientJoint("bn_", rigData["ArmJoints"][0][0], rigData["ArmJoints"][0][2], vecList[0], vecList[2])
+        self.arm_orientJoint("bn_", rigData["ArmJoints"][0][2], rigData["ArmJoints"][0][3], vecList[0], vecList[2])
 
-        self.arm_orientJoint("IK_"+rigData["ArmJoints"][0][0],"IK_"+rigData["ArmJoints"][0][1], vecList[0], vecList[2])
-        self.arm_orientJoint("IK_"+rigData["ArmJoints"][0][1],"IK_"+rigData["ArmJoints"][0][2], vecList[0], vecList[2])
-        self.arm_orientJoint("IK_"+rigData["ArmJoints"][0][2],"IK_"+rigData["ArmJoints"][0][3], vecList[0], vecList[2])
+        self.arm_orientJoint("IK_", rigData["ArmJoints"][0][0], rigData["ArmJoints"][0][2], vecList[0], vecList[2])
+        self.arm_orientJoint("IK_", rigData["ArmJoints"][0][2], rigData["ArmJoints"][0][3], vecList[0], vecList[2])
 
-        self.arm_orientJoint("FK_"+rigData["ArmJoints"][0][0],"FK_"+rigData["ArmJoints"][0][1], vecList[0], vecList[2])
-        self.arm_orientJoint("FK_"+rigData["ArmJoints"][0][1],"FK_"+rigData["ArmJoints"][0][2], vecList[0], vecList[2])
-        self.arm_orientJoint("FK_"+rigData["ArmJoints"][0][2],"FK_"+rigData["ArmJoints"][0][3], vecList[0], vecList[2])
+        self.arm_orientJoint("FK_", rigData["ArmJoints"][0][0], rigData["ArmJoints"][0][2], vecList[0], vecList[2])
+        self.arm_orientJoint("FK_", rigData["ArmJoints"][0][2], rigData["ArmJoints"][0][3], vecList[0], vecList[2])
 
-        self.arm_orientJoint("IK_"+rigData["ArmJoints"][1][0],"IK_"+rigData["ArmJoints"][1][1], vecList[1], vecList[3])
-        self.arm_orientJoint("IK_"+rigData["ArmJoints"][1][1],"IK_"+rigData["ArmJoints"][1][2], vecList[1], vecList[3])
-        self.arm_orientJoint("IK_"+rigData["ArmJoints"][1][2],"IK_"+rigData["ArmJoints"][1][3], vecList[1], vecList[3])
+        self.arm_orientJoint("IK_", rigData["ArmJoints"][1][0], rigData["ArmJoints"][1][2], vecList[1], vecList[3])
+        self.arm_orientJoint("IK_", rigData["ArmJoints"][1][2], rigData["ArmJoints"][1][3], vecList[1], vecList[3])
 
-        self.arm_orientJoint("bn_"+rigData["ArmJoints"][1][0],"bn_"+rigData["ArmJoints"][1][1], vecList[1], vecList[3])
-        self.arm_orientJoint("bn_"+rigData["ArmJoints"][1][1],"bn_"+rigData["ArmJoints"][1][2], vecList[1], vecList[3])
-        self.arm_orientJoint("bn_"+rigData["ArmJoints"][1][2],"bn_"+rigData["ArmJoints"][1][3], vecList[1], vecList[3])
+        self.arm_orientJoint("bn_", rigData["ArmJoints"][1][0], rigData["ArmJoints"][1][2], vecList[1], vecList[3])
+        self.arm_orientJoint("bn_", rigData["ArmJoints"][1][2], rigData["ArmJoints"][1][3], vecList[1], vecList[3])
 
-        self.arm_orientJoint("FK_"+rigData["ArmJoints"][1][0],"FK_"+rigData["ArmJoints"][1][1], vecList[1], vecList[3])
-        self.arm_orientJoint("FK_"+rigData["ArmJoints"][1][1],"FK_"+rigData["ArmJoints"][1][2], vecList[1], vecList[3])
-        self.arm_orientJoint("FK_"+rigData["ArmJoints"][1][2],"FK_"+rigData["ArmJoints"][1][3], vecList[1], vecList[3])
+        self.arm_orientJoint("FK_", rigData["ArmJoints"][1][0], rigData["ArmJoints"][1][2], vecList[1], vecList[3])
+        self.arm_orientJoint("FK_", rigData["ArmJoints"][1][2], rigData["ArmJoints"][1][3], vecList[1], vecList[3])
 
-            #Cleaning Joint Orient 
         self.cleanJntOrientArm("IK_")
         self.cleanJntOrientArm("FK_")
         self.cleanJntOrientArm("bn_")
-        
-            #Making IK
+
+
         self.IK_Create()
-            
-            #Making IK Controls
         self.IK_CtrlMaker()
-            # PV Position 
-        
-        lf_pvpos = self.calculatePVPosition(["IK_"+rigData["ArmJoints"][0][0], "IK_"+rigData["ArmJoints"][0][1], "IK_"+rigData["ArmJoints"][0][2]])
-        lf_pvctrlinfo = [
-        [lf_pvpos, rigData["IK_ArmList"][0][4], rigData["IK_ArmList"][0][5]]
-        ]
-        rt_pvpos = self.calculatePVPosition(["IK_"+rigData["ArmJoints"][1][0], "IK_"+rigData["ArmJoints"][1][1], "IK_"+rigData["ArmJoints"][1][2]])
-        rt_pvctrlinfo = [
-        [rt_pvpos, rigData["IK_ArmList"][1][4], rigData["IK_ArmList"][1][5]]
-        ]
+        lf_pvpos = self.calculatePVPosition(["IK_"+rigData["ArmJoints"][0][1], "IK_"+rigData["ArmJoints"][0][2], "IK_"+rigData["ArmJoints"][0][3]])
+        lf_pvctrlinfo = lf_pvpos
+        rt_pvpos = self.calculatePVPosition(["IK_"+rigData["ArmJoints"][1][1], "IK_"+rigData["ArmJoints"][1][2], "IK_"+rigData["ArmJoints"][1][3]])
+        rt_pvctrlinfo = rt_pvpos
+        self.PV_CtrlShape()
+        self.PV_CtrlMaker(lf_pvctrlinfo, rt_pvctrlinfo)
         self.FK_CtrlShape()
         self.FK_CtrlMaker()
+    
+
     def HrcSetup(self):
          cmds.group(em = True, name = rigData["HierarchyOrg"][0])
          cmds.group(em = True, p = rigData["HierarchyOrg"][0], name = rigData["HierarchyOrg"][1])
          cmds.group(em = True, p = rigData["HierarchyOrg"][0], name = rigData["HierarchyOrg"][2])
          cmds.group(em = True, p = rigData["HierarchyOrg"][2], name = rigData["HierarchyOrg"][3])
-         cmds.group(em = True, p = rigData["HierarchyOrg"][3], name = rigData["HierarchyOrg"][4])
+         cmds.group(em = True, p = rigData["HierarchyOrg"][3], name = "bn_"+rigData["HierarchyOrg"][4])
+         cmds.group(em = True, p = rigData["HierarchyOrg"][3], name = "IK_"+rigData["HierarchyOrg"][4])
+         cmds.group(em = True, p = rigData["HierarchyOrg"][3], name = "FK_"+rigData["HierarchyOrg"][4])
          cmds.group(em = True, p = rigData["HierarchyOrg"][3], name = rigData["HierarchyOrg"][5])
-         cmds.group(em = True, p = rigData["HierarchyOrg"][3], name = rigData["HierarchyOrg"][6])
-         cmds.group(em = True, p = rigData["HierarchyOrg"][3], name = rigData["HierarchyOrg"][7])
-         cmds.group(em = True, p = rigData["HierarchyOrg"][2], name = rigData["HierarchyOrg"][8])
-         cmds.group(em = True, p = rigData["HierarchyOrg"][0], name = rigData["HierarchyOrg"][9])
-         cmds.group(em = True, p = rigData["HierarchyOrg"][9], name = rigData["HierarchyOrg"][10])
-         cmds.group(em = True, p = rigData["HierarchyOrg"][9], name = rigData["HierarchyOrg"][11])    
+         cmds.group(em = True, p = rigData["HierarchyOrg"][2], name = rigData["HierarchyOrg"][6])
+         cmds.group(em = True, p = rigData["HierarchyOrg"][0], name = rigData["HierarchyOrg"][7])
+         cmds.group(em = True, p = rigData["HierarchyOrg"][7], name = rigData["HierarchyOrg"][8])
+         cmds.group(em = True, p = rigData["HierarchyOrg"][7], name = rigData["HierarchyOrg"][9])
+         cmds.group(em = True, p = rigData["HierarchyOrg"][3], name = rigData["HierarchyOrg"][10])    
     #Building joints___Function
     def createArmJnt(self, jntType):
         for i in range(len(rigData["ArmJoints"])):
-            for o in range(len(rigData["ArmJoints"][i])):
-                cmds.joint(n=jntType+rigData["ArmJoints"][i][o], p=rigData["JointsPos"][i][o])        
-                cmds.select(cl=True)
+                for o in range(len(rigData["ArmJoints"][i])):
+                    if o==0:
+                        cmds.joint(n=jntType+rigData["ArmJoints"][i][o], p=rigData["JointsPos"][i][0])
+                    else:
+                        cmds.joint(n=jntType+rigData["ArmJoints"][i][o], p=rigData["JointsPos"][i][o-1])
+                    cmds.select(cl=True)
                 
     #Orient Joints____Function
-    def arm_orientJoint(self, orntJnt, aimJnt, aimVec, upVec):
+    def arm_orientJoint(jntType, orntJnt, aimJnt, aimVec, upVec):
         loc = cmds.spaceLocator()
-        cmds.parent(loc[0], orntJnt)
+        cmds.parent(loc[0], jntType+orntJnt)
         cmds.setAttr(loc[0]+".translate", 0, 0, 5, type = "double3")
         cmds.parent(loc[0], w=True)
-        cmds.aimConstraint(aimJnt, orntJnt, offset = [0, 0, 0], weight = True, aimVector = aimVec ,
+        cmds.aimConstraint(jntType+aimJnt, jntType+orntJnt, offset = [0, 0, 0], weight = True, aimVector = aimVec ,
                                                      upVector = upVec, worldUpType = "object" , worldUpObject = loc[0])
-        cmds.delete(orntJnt+"_aimConstraint1")
-        cmds.makeIdentity(orntJnt, apply = True, t = 0,  r = 1, s = 0, n = 0, pn = True)
+        cmds.delete(jntType+orntJnt+"_aimConstraint1")
+        cmds.makeIdentity(jntType+orntJnt, apply = True, t = 0,  r = 1, s = 0, n = 0, pn = True)
         cmds.delete(loc[0])
-        cmds.parent(aimJnt, orntJnt)
          
         
 
     #Cleaning up joint orinet___Function    
     def cleanJntOrientArm(self, jntType):
         for i in range(len(rigData["ArmJoints"])):
-            for o in range(len(rigData["ArmJoints"][i])):
-                if rigData["ArmJoints"][i][o] == rigData["ArmJoints"][0][0] or rigData["ArmJoints"][i][o] == rigData["ArmJoints"][1][0]:
-                    pass
-                elif rigData["ArmJoints"][i][o] == rigData["ArmJoints"][0][3] or rigData["ArmJoints"][i][o] == rigData["ArmJoints"][1][3]:
-                    cmds.delete(jntType+rigData["ArmJoints"][i][o])
-                    
-                elif rigData["ArmJoints"][i][o] == rigData["ArmJoints"][0][2] or rigData["ArmJoints"][i][o] == rigData["ArmJoints"][1][2]:
-                    cmds.setAttr(jntType+rigData["ArmJoints"][i][o] +'.jointOrientX', 0)    
-                else:
-                    cmds.setAttr(jntType+rigData["ArmJoints"][i][o] +".jointOrientX", 0)
-                    cmds.setAttr(jntType+rigData["ArmJoints"][i][o] +".jointOrientY", 0)
+            #Putting in hrc foulder base shoulder
+            cmds.parent(jntType+rigData["ArmJoints"][i][0], jntType+rigData["HierarchyOrg"][4])
+            # parenting shoulder to base shoulder and cleaninng
+            cmds.parent(jntType+rigData["ArmJoints"][i][1], jntType+rigData["ArmJoints"][i][0])
+            cmds.setAttr(jntType+rigData["ArmJoints"][i][1]+".jointOrient", 0, 0, 0, type="double3")
+            # parent elbow to shoulder and cleaning
+            cmds.parent(jntType+rigData["ArmJoints"][i][2], jntType+rigData["ArmJoints"][i][1])
+            cmds.setAttr(jntType+rigData["ArmJoints"][i][2]+".jointOrientX", 0)
+            cmds.setAttr(jntType+rigData["ArmJoints"][i][2]+".jointOrientY", 0)
+            # parent wrist to elbow and cleaning
+            cmds.parent(jntType+rigData["ArmJoints"][i][3], jntType+rigData["ArmJoints"][i][2])
+            cmds.setAttr(jntType+rigData["ArmJoints"][i][3]+".jointOrient", 0, 0, 0, type="double3")
 
     #Making Controls___Function
     
@@ -140,22 +131,17 @@ class Rig_Arm:
             cmds.xform(ctrlGRP, t=pos, ws = True)
             cmds.parent(ctrlGRP, rigData["IK_ArmList"][i][2])
             cmds.makeIdentity(ctrlGRP, apply = True, t = 0, r = 1, s = 0, n = 0, pn = True)
-            cmds.parent(ctrlGRP, rigData["HierarchyOrg"][8])
+            cmds.parent(ctrlGRP, rigData["HierarchyOrg"][6])
+            cmds.pointConstraint(rigData["IK_ArmList"][i][0], rigData["IK_ArmList"][i][2])
 
     def IK_Create(self):
-        lf_IKH = cmds.ikHandle( n = rigData["IK_ArmList"][0][2] , sj = "IK_"+rigData["ArmJoints"][0][0], ee = "IK_" + rigData["ArmJoints"][0][2])
-        lf_IKE = cmds.rename(lf_IKH[1], rigData["IK_ArmList"][0][3])
-        rt_IKH = cmds.ikHandle( n = rigData["IK_ArmList"][1][2] , sj = "IK_"+rigData["ArmJoints"][1][0], ee = "IK_" + rigData["ArmJoints"][1][2])
-        rt_IKE = cmds.rename(rt_IKH[1], rigData["IK_ArmList"][1][3])
-
-        #Orienting IK Handle
-        cmds.parent(lf_IKH[0], "bn_"+rigData["ArmJoints"][0][2])
-        cmds.makeIdentity(lf_IKH[0], apply = True, t = 0,  r = 1, s = 0, n = 0, pn = True)
-        cmds.parent(lf_IKH[0], w = True)
-        
-        cmds.parent(rt_IKH[0], "bn_"+rigData["ArmJoints"][1][2])
-        cmds.makeIdentity(rt_IKH[0], apply = True, t = 0,  r = 1, s = 0, n = 0, pn = True)
-        cmds.parent(rt_IKH[0], w = True)   
+        for i in range(len(rigData["IK_ArmList"])):      
+            IKH = cmds.ikHandle( n = rigData["IK_ArmList"][i][2] , sj = "IK_"+rigData["ArmJoints"][i][1], ee = "IK_" + rigData["ArmJoints"][i][3])
+            IKE = cmds.rename(IKH[1], rigData["IK_ArmList"][i][3])
+            #Orienting IK Handle
+            cmds.parent(IKH[0], "bn_"+rigData["ArmJoints"][i][3])
+            cmds.makeIdentity(IKH[0], apply = True, t = 0,  r = 1, s = 0, n = 0, pn = True)
+            cmds.parent(IKH[0], rigData["HierarchyOrg"][10])   
     
     def calculatePVPosition(self, jnts):
         from maya import cmds, OpenMaya
@@ -183,10 +169,27 @@ class Rig_Arm:
                 ctrlGRP = cmds.group(em = True, name = rigData["FK_ArmList"][o][1::2][s])
                 cmds.parent(rigData["FK_ArmList"][o][0::2][s], ctrlGRP)
                 cmds.xform(ctrlGRP, t=pos, ws = True)
-                cmds.parent(ctrlGRP, "FK_"+rigData["ArmJoints"][o][s])   
+                cmds.parent(ctrlGRP, "FK_"+rigData["ArmJoints"][o][s+1])   
                 cmds.makeIdentity(ctrlGRP, apply=True, t=0, r=1, s=0, n=0, pn=True)
-                cmds.parent(ctrlGRP, rigData["HierarchyOrg"][8])
-     
+                if s!=0:
+                    cmds.parent(ctrlGRP, rigData["FK_ArmList"][o][0::2][s-1])
+                else:
+                    cmds.parent(ctrlGRP, rigData["HierarchyOrg"][6])
+                cmds.orientConstraint(rigData["FK_ArmList"][o][0::2][s], "FK_"+rigData["ArmJoints"][o][s+1])
+    def PV_CtrlMaker(self, lf_position, rt_position):
+        for i in range(len(rigData["IK_ArmList"])):
+            if i==0:
+                pos = lf_position
+            else:
+                pos = rt_position
+            ctrlGRP = cmds.group(em = True, name = rigData["IK_ArmList"][i][5])
+           
+            cmds.parent(rigData["IK_ArmList"][i][4], ctrlGRP)
+            cmds.xform(ctrlGRP, t=pos, ws=True)
+            cmds.parent(ctrlGRP, rigData["HierarchyOrg"][6])
+            wPos = cmds.getAttr(rigData["IK_ArmList"][i][5]+".tz")
+            cmds.setAttr(rigData["IK_ArmList"][i][5]+".tz", wPos-20)
+
     def FK_CtrlShape(self):
         for i in range(len(rigData["FK_ArmList"])):
             for o in range(len(rigData["FK_ArmList"][i][0::2])):            
@@ -259,14 +262,19 @@ class Rig_Arm:
                 cmds.rename(crv, rigData["FK_ArmList"][i][0::2][o])
 
     def PV_CtrlShape(self):
-        cmds.curve( degree = 1,\
-                    knot = [0, 1, 2, 3, 4, 5, 6, 7],\
-                    point = [(-2, 0, 0),\
-                             (1, 0, 1),\
-                             (1, 0, -1),\
-                             (-2, 0, 0),\
-                             (1, 1, 0),\
-                             (1, 0, 0),\
-                             (1, -1, 0),\
-                             (-2, 0, 0)]\
-                             )
+        for i in range(len(rigData["IK_ArmList"])):      
+            crv = cmds.curve( degree = 1,\
+                        knot = [0, 1, 2, 3, 4, 5, 6, 7],\
+                        point = [(-2, 0, 0),\
+                                 (1, 0, 1),\
+                                 (1, 0, -1),\
+                                 (-2, 0, 0),\
+                                 (1, 1, 0),\
+                                 (1, 0, 0),\
+                                 (1, -1, 0),\
+                                 (-2, 0, 0)]\
+                      )
+            
+            cmds.setAttr(crv+".ry", -90)             
+            cmds.makeIdentity(crv, apply = True, t = 0,  r = 1, s = 0, n = 0, pn = True)
+            cmds.rename(crv, rigData["IK_ArmList"][i][4])
